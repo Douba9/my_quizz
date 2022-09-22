2d66a2ec8b2905599f1a5c64e51c3ae08ca3c02a
marius <marius.mestron@epitech.eu>
2022-09-22 12:31:44 +0200

    Affichage des catégorie de quizz

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,20 +12,27 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $rowNo = 1;
+        $category = [];
 
         if (($fp = fopen("/home/marius/delivery/my_quizz/QuizzApp/src/Controller/questions.csv", "r")) !== FALSE) {
-        while (($row = fgetcsv($fp, 1000, ",")) !== FALSE) {
-        $num = count($row);
-        $rowNo++;
-        for ($c=0; $c < $num; $c++) {
-        echo $row[$c] . "<br />\n";
-        }
-        }
-        fclose($fp);
+            while (($row = fgetcsv($fp, 1000, ",")) !== FALSE) {
+                $num = count($row);
+                $rowNo++;
+                for ($c = 0; $c < $num; $c++) {
+                    //echo $row[$c] . "<br />\n";
+                    if(str_contains($row[$c], ";;;")){
+                        if($row[$c] != ";;;"){
+                            array_push($category, trim($row[$c], ";"));
+                        }
+                    }
+                }
+            }
+            fclose($fp);
         }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'category' => $category,
         ]);
     }
 }
