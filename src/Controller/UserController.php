20abe014172593fc08cd6e0fd56e51ae7eaf4509
amine <mohammed-el-amine.djellal@epitech.eu>
2022-09-28 12:19:14 +0200

    [UPDATE] template admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Subscribe;
+use App\Form\SubscribeType;
+use App\Repository\SubscribeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,26 @@ class UserController extends AbstractController
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    
+
+    #[Route('/user/{username}', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Subscribe $subscribe, SubscribeRepository $subscribeRepository): Response
+    {
+        $form = $this->createForm(SubscribeType::class, $subscribe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subscribeRepository->add($subscribe, true);
+
+            return $this->redirectToRoute('app_user', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'subscribe' => $subscribe,
+            'form' => $form,
         ]);
     }
 }
