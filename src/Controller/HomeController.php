58723ddd8d1049b3eda2ee6a496de147c1f82210
marius <marius.mestron@epitech.eu>
2022-09-29 11:16:26 +0200

    .

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,27 +11,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $rowNo = 1;
-        $category = [];
-
-        if (($fp = fopen("/home/marius/delivery/my_quizz/QuizzApp/src/Controller/questions.csv", "r")) !== FALSE) {
-            while (($row = fgetcsv($fp, 1000, ",")) !== FALSE) {
-                $num = count($row);
-                $rowNo++;
-                for ($c = 0; $c < $num; $c++) {
-                    if(str_contains($row[$c], ";;;")){
-                        if($row[$c] != ";;;"){
-                            array_push($category, utf8_encode(trim($row[$c], ";")));
-                        }
-                    }
-                }
-            }
-            fclose($fp);
-        }
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'category' => $category,
         ]);
     }
 }
